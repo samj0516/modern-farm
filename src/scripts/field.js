18ed883a3f4a1f0ffd2fed35d32b4c47e4cb1dad
samj0516 <samj0516@gmail.com>
2021-01-18 15:39:38 -0600

    fixed corn array from being pushed instead of corn object in field.js

--- src/scripts/field.js
+++ src/scripts/field.js
@@ -3,7 +3,11 @@ const field = []
 
 // adds each vegetable object/array to the field array 
 export const addPlant = (seed) => {
+    if (Array.isArray(seed)){
+        seed.forEach(element => field.push(element))
+    } else {
     field.push(seed)
+    }
 }
 // seed is a vegetable object
 // export const createAsparagus = () => {
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,12 +4,7 @@ import { createPlan } from './plan.js'
 const yearlyPlan = createPlan()
 console.log(yearlyPlan)
 
-// import { createAsparagus } from './seeds/asparagus.js'
-// import { createCorn } from './seeds/corn.js'
-// import { createPotato } from './seeds/potato.js'
-// import { createSoybean } from './seeds/soybean.js'
-// import { createSunflower } from './seeds/sunflower.js'
-// import { createWheat } from './seeds/wheat.js'
+
 // const asparagusSeed = createAsparagus()
 // console.log(asparagusSeed)
 // const cornSeed = createCorn()
