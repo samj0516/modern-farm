79a24bcb15f37c86a4d4fabf967cf4433e459d8b
samj0516 <samj0516@gmail.com>
2021-01-19 00:08:40 -0600

    figured out corn exception on harvester

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -3,7 +3,7 @@ let harvestArray = []
 export const harvestPlants = (fieldArray) => {
     for (const veggie of fieldArray) {
         let copy = veggie.output
-        if (veggie == "Corn"){
+        if (veggie.type == "Corn"){
          let cornCopy = copy / 2
             for (let i = 0; i < cornCopy; i++){
                 harvestArray.push(veggie)
